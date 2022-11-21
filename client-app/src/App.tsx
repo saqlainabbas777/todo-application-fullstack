9756e181d94dad183e8fd32ddb0c980975b54e22
saqlainAbbas <saqlain.abbas@narsun.pk>
2022-11-21 12:19:43 +0500

    -> mui init -> customTheme.ts init -> dashboard.tsx init

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,7 +1,15 @@
 import React, {FC, ReactElement} from 'react';
+import {ThemeProvider, CssBaseline} from "@mui/material";
+import {customTheme} from "./theme/customTheme";
+import {Dashboard} from "./pages/dashboard/dashboard";
 
-const App = () => {
- return <h1>hello world</h1>;
+const App: FC = (): ReactElement => {
+    return (
+        <ThemeProvider theme={customTheme}>
+            <CssBaseline/>
+            <Dashboard/>
+        </ThemeProvider>
+    );
 }
 
 export default App;
